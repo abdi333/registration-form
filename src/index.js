7f6dcaca93ebf4c8b54b90b6c5f043cf912e29f1
Abdi Hirsi <abdi.omer33@gmail.com>
2022-11-29 18:05:10 -0500

    updates to registration form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
+
+
+function App() {
+
+    const [email, setEmail] = useState("");
+    const emailEl = useRef();
+
+    const [first, setFirst] = useState("");
+    const firstEl = useRef();
+
+    const [last, setLast] = useState("");    
+    const lastEl = useRef();
+
+
+
+    return (
+        <div>
+            <h1>Form</h1>
+            <h4>
+            <label htmlFor="email">Your Email Address</label>
+            <br />
+            <input type="text" name="email" ref={emailEl} />
+            <br />
+
+            <label htmlFor="first-name">Your First Name :</label>
+            <br />
+            <input type="text" name="first-name" ref={firstEl} />
+            <br />
+
+            <label htmlFor="last-name">Your Last Name :</label>
+            <br />
+            <input type="text" name="last-name" ref={lastEl} />
+            </h4>
+            <br />
+            <br />
+            <br />
+
+            <button onClick={function () {
+                setEmail(emailEl.current.value); setFirst(firstEl.current.value); setLast(lastEl.current.value)}}>Submit the form</button>
+            <br />
+            <br />
+            <br />
+
+            <div>
+                <h4>
+                Your Email Adress is : {email} <br /><br />
+                Your First Name : {first} <br /><br />
+                Your Last Name : {last}
+                </h4>
+            </div>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
